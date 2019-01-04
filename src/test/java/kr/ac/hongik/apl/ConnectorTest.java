55e28037b1dba0f1fdc9e850693a2a13adce15c0
Moon Junoh <mjo970625@gmail.com>
2019-01-05 01:11:38 +0900

    Remove ignored files

---
+++ src/test/java/kr/ac/hongik/apl/ConnectorTest.java
@@ -0,0 +1,14 @@
+package kr.ac.hongik.apl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConnectorTest {
+
+    @org.junit.jupiter.api.Test
+    void serialize() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deserialize() {
+    }
+}
