7d96b2d37f8b59ede4804ee8d4e03f8de7a9fa56
Moon Junoh <mjo970625@gmail.com>
2019-01-07 21:30:40 +0900

    Change ip address object from String to InetAddress

--- src/main/java/kr/ac/hongik/apl/Endpoint.java
+++ src/main/java/kr/ac/hongik/apl/Endpoint.java
@@ -1,11 +1,21 @@
 package kr.ac.hongik.apl;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Endpoint {
-    public final String ip;
+    public final InetAddress ip;
     public final int port;
 
     public Endpoint(String ip, int port){
-        this.ip = ip;
+        InetAddress tmpIp;
+        try {
+            tmpIp = InetAddress.getByName(ip);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            tmpIp = null;
+        }
+        this.ip = tmpIp;
         this.port = port;
     }
 }
