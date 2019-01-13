50aa520a9dc7597f4ee7fa31336f7318e403ea30
Moon Junoh <mjo970625@gmail.com>
2019-01-14 01:06:46 +0900

    Test and verify that making tables is successful

--- src/main/java/kr/ac/hongik/apl/Logger.java
+++ src/main/java/kr/ac/hongik/apl/Logger.java
@@ -8,39 +8,59 @@ import java.sql.SQLException;
 public class Logger {
     private Connection conn = null;
 
-    /**
-     * Schema Design
-     * ^ symbol means it is a prime attribute.
-     * Request: (^client, ^timestamp, ^message). Insert this triple after its signature is validated. Primary only.
-     * Preprepare: (^viewNum, ^seqNum, ^digest, fullRequest)
-     * Prepare: (^viewNum, ^seqNum, ^digest, ^i)
-     * Commit: (^viewNum, ^seqNum, ^digest, ^i)
-     * Checkpoint(^seqNum, ^stateDigest, ^i)
-     */
+
     public Logger() {
-        String url = "jdbc;sqlite:src/main/resources/" + this.toString() + ".db";
+        String url = "jdbc:sqlite:src/main/resources/" + this.toString() + ".db";
         try {
             this.conn= DriverManager.getConnection(url);
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        this.createTables();
+    }
+
+    /**
+     * Schema Design
+     * ^ symbol means it is a prime attribute.
+     * Requests:    (^client, ^timestamp, ^message). Insert this triple after its signature is validated. Primary only.
+     * Preprepares: (^viewNum, ^seqNum, ^digest, fullRequest)
+     * Prepares:    (^viewNum, ^seqNum, ^digest, ^i)
+     * Commits:     (^viewNum, ^seqNum, ^digest, ^i)
+     * Checkpoints: (^seqNum, ^stateDigest, ^i)
+     */
+    private void createTables(){
         String[] querys = {
-                "CREATE TABLE Request (client BLOB,timestamp DATE,message BLOB, PRIMARY KEY(client, timestamp, message))",
-                "CREATE TABLE Preprepare (viewNum INT, seqNum INT, digest TEXT, message BLOB, PRIMARY KEY(viewNum, seqNum, digest))",
-                "CREATE TABLE Prepare (viewNum INT, seqNum INT, digest TEXT, replica INT, PRIMARY KEY(viewNum, seqNum, digest, replica))",
-                "CREATE TABLE Commit (viewNum INT, seqNum INT, digest TEXT, replica INT, PRIMARY KEY(viewNum, seqNum, digest, replica))",
-                "CREATE TABLE Checkpoint (seqNum INT, stateDigest TEXT, replica INT, PRIMARY KEY(seqNum, stateDigest, replica))",
+                "CREATE TABLE Requests (client BLOB,timestamp DATE,message BLOB, PRIMARY KEY(client, timestamp, message))",
+                "CREATE TABLE Preprepares (viewNum INT, seqNum INT, digest TEXT, message BLOB, PRIMARY KEY(viewNum, seqNum, digest))",
+                "CREATE TABLE Prepares (viewNum INT, seqNum INT, digest TEXT, replica INT, PRIMARY KEY(viewNum, seqNum, digest, replica))",
+                "CREATE TABLE Commits (viewNum INT, seqNum INT, digest TEXT, replica INT, PRIMARY KEY(seqNum, replica))",
+                "CREATE TABLE Checkpoints (seqNum INT, stateDigest TEXT, replica INT, PRIMARY KEY(seqNum, stateDigest, replica))",
         };
-        try {
             for(String query : querys){
-                PreparedStatement preparedStatement = conn.prepareStatement(query);
-                preparedStatement.execute();
+                try {
+                    PreparedStatement preparedStatement = conn.prepareStatement(query);
+                    preparedStatement.execute();
+                } catch (SQLException e) {
+                    System.err.println(query);
+                    System.err.println(e.getSQLState());
+                    e.printStackTrace();
+                }
             }
+    }
+
+    public void close(){
+        try {
+            conn.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    void InsertMessage(Message message){
+    void insertMessage(Message message){
+    }
+
+    boolean searchMessage(Message message){
+
+        return true;
     }
 }
---
+++ src/test/java/kr/ac/hongik/apl/LoggerTest.java
@@ -0,0 +1,11 @@
+package kr.ac.hongik.apl;
+
+import org.junit.jupiter.api.Test;
+
+class LoggerTest {
+
+    @Test
+    void createTables() {
+        Logger logger = new Logger();
+    }
+}
