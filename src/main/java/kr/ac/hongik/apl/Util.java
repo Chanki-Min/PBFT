c6040a9bd009aec9c86c2c8dd2223c7b74ceb5db
문준오 <mjo970625@gmail.com>
2019-01-14 15:31:09 +0900

    Change default message type from Message to Serializable

--- src/main/java/kr/ac/hongik/apl/Util.java
+++ src/main/java/kr/ac/hongik/apl/Util.java
@@ -1,7 +1,82 @@
 package kr.ac.hongik.apl;
 
+import java.io.*;
+import java.security.*;
+import java.util.Formatter;
+import java.util.stream.IntStream;
+
 public class Util {
+    public static String hash(Serializable obj) {
+        return hash(serialize(obj));
+    }
+    public static String hash(final byte[] bytes) {
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return bytesToHex(digest.digest(bytes));
+    }
+    public static String bytesToHex(final byte[] bytes) {
+        return IntStream.range(0, bytes.length)
+                .collect(StringBuilder::new,
+                        (sb, i) -> new Formatter(sb).format("%02x", bytes[i] & 0xFF),
+                        StringBuilder::append).toString();
+    }
+     public static byte[] serialize(Serializable message){
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(out);
+            outputStream.writeObject(message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
+        return out.toByteArray();
+    }
 
+    public static Serializable deserialize(byte[] bytes){
+        ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+        Serializable object = null;
+        try {
+            ObjectInputStream inputStream = new ObjectInputStream(in);
+            object = (Serializable)inputStream.readObject();
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return object;
+    }
+
+    /**
+     * Return (private, public) key pair.
+     * To get each keys, use KeyPair.getPublic and KeyPair.getPrivate methods.
+     * @return KeyPair object
+     * @throws NoSuchAlgorithmException
+     */
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        return generator.generateKeyPair();
+    }
+
+    public static byte[] sign(PrivateKey key, Serializable message) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException, NoSuchProviderException {
+        Signature signature = Signature.getInstance("SHA1withRSA");
+        signature.initSign(key);
+        signature.update(serialize(message));
+        return signature.sign();
+    }
+    public static boolean verify(PublicKey key, Serializable message, byte[] signatureBytes) {
+        try {
+            Signature signature = Signature.getInstance("SHA1withRSA");
+            signature.initVerify(key);
+            signature.update(serialize(message));
+            return signature.verify(signatureBytes);
+        } catch (NoSuchAlgorithmException | SignatureException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 }
