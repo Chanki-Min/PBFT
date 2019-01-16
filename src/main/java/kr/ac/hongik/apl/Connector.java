172d945ba805ff650120f6112a75acc4eb371d9b
Moon Junoh <mjo970625@gmail.com>
2019-01-16 22:34:12 +0900

    Sockets and addresses match regardless of reconnection
    Plus, makeConnections and send functions are refactored.

--- src/main/java/kr/ac/hongik/apl/Connector.java
+++ src/main/java/kr/ac/hongik/apl/Connector.java
@@ -11,6 +11,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Properties;
+import java.util.stream.Collectors;
 
 import static kr.ac.hongik.apl.Util.deserialize;
 import static kr.ac.hongik.apl.Util.serialize;
@@ -19,127 +20,131 @@ import static kr.ac.hongik.apl.Util.serialize;
  * Caution: It doesn't handling server's listening socket
  */
 abstract class Connector {
-    final static int bufferSize = 16 * 1024 * 1024;
-    protected List<InetSocketAddress> addresses;
-    protected List<SocketChannel> sockets;
-    protected Selector selector;
+	final static int bufferSize = 16 * 1024 * 1024;
+	//Invariant: replica index and its socket is matched!
+	protected List<InetSocketAddress> addresses;
+	protected List<SocketChannel> sockets;
+	protected Selector selector;
 
-    int numOfReplica;
+	int numOfReplica;
 
-    public Connector() {
-    }
+	public Connector() {
+	}
 
-    public Connector(Properties prop) {
-        numOfReplica = Integer.parseInt(prop.getProperty("replica"));
+	public Connector(Properties prop) {
+		numOfReplica = Integer.parseInt(prop.getProperty("replica"));
 
-        addresses = new ArrayList<>();
-        for (int i = 0; i < numOfReplica; i++) {
-            String addressInString = prop.getProperty("replica" + i);
-            String[] parsedAddress = addressInString.split(":");
+		addresses = new ArrayList<>();
+		for (int i = 0; i < numOfReplica; i++) {
+			String addressInString = prop.getProperty("replica" + i);
+			String[] parsedAddress = addressInString.split(":");
 
-            InetSocketAddress address = new InetSocketAddress(parsedAddress[0], Integer.parseInt(parsedAddress[1]));
-            addresses.add(address);
-        }
+			InetSocketAddress address = new InetSocketAddress(parsedAddress[0], Integer.parseInt(parsedAddress[1]));
+			addresses.add(address);
+		}
 
-        try {
-            this.selector = Selector.open();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		try {
+			this.selector = Selector.open();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        makeConnections();
-    }
+		makeConnections();
+	}
 
-    private void makeConnections() {
-        //Connect to every replica
-        sockets = new ArrayList<>();
-        addresses.stream().forEach(x -> sockets.add(makeConnectionOrNull(x)));
+	private void makeConnections() {
+		//Connect to every replica
+		sockets = addresses.stream()
+				.map(address -> {
+					var socket = makeConnectionOrNull(address);
+					try {
+						socket.register(this.selector, SelectionKey.OP_READ);
+					} catch (ClosedChannelException | NullPointerException e) {
+						e.printStackTrace();
+					}
+					return socket;
+				}).collect(Collectors.toList());
+	}
 
-        sockets.stream().forEach(socket -> {
-            try {
-                socket.register(this.selector, SelectionKey.OP_READ);
-            } catch (ClosedChannelException e) {
-                e.printStackTrace();
-            }
-        });
-    }
+	private void closeWithoutException(SocketChannel socketChannel) {
+		try {
+			socketChannel.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private SocketChannel makeConnectionOrNull(InetSocketAddress address) {
-        try {
-            SocketChannel socket = SocketChannel.open(address);
-            return socket;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	private SocketChannel makeConnectionOrNull(InetSocketAddress address) {
+		try {
+			SocketChannel socket = SocketChannel.open(address);
+			return socket;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    /**
-     * It is a exception free wrapper of send function.
-     * It sends the data and also manage the wrong sockets
-     * The endpoint object must be in the sockets list, or this function does nothing.
-     *
-     * @param address
-     * @param message
-     */
-    protected void send(InetSocketAddress address, Message message) {
-        ByteBuffer serializedMessage = ByteBuffer.wrap(serialize(message));
+	/**
+	 * It is a exception free wrapper of send function.
+	 * It sends the data and also manage the wrong sockets
+	 * The endpoint object must be in the sockets list, or this function does nothing.
+	 * <p>
+	 * Plus, the order of sockets are maintained while reconnecting
+	 *
+	 * @param destination
+	 * @param message
+	 */
+	protected void send(InetSocketAddress destination, Message message) {
+		ByteBuffer serializedMessage = ByteBuffer.wrap(serialize(message));
 
-        for (SocketChannel socket : sockets) {
-            try {
-                if (socket.getRemoteAddress().equals(address)) {
-                    socket.write(serializedMessage);
-                }
-            } catch (IOException | NullPointerException e) {
-                e.printStackTrace();
-                //Close previous connection
-                try {
-                    socket.close();
-                } catch (IOException e1) {
-                    e1.printStackTrace();
-                }
-                sockets.remove(socket);
-                //Reconnect
-                sockets.add(makeConnectionOrNull(address));
-            }
-        }
-    }
+		sockets = sockets.stream().map(socket -> {
+			try {
+				if (socket.getRemoteAddress().equals(destination))
+					socket.write(serializedMessage);
+				return socket;
+			} catch (IOException | NullPointerException e) {
+				e.printStackTrace();
+				closeWithoutException(socket);
+				return makeConnectionOrNull(destination);
+			}
+		}).collect(Collectors.toList());
+	}
 
-    /**
-     * This abstract method must be implemented in Replica class to handle Accept situation.
-     *
-     * @param key
-     */
-    protected abstract void acceptOp(SelectionKey key);
+	/**
+	 * This abstract method must be implemented in Replica class to handle Accept situation.
+	 *
+	 * @param key
+	 */
+	protected abstract void acceptOp(SelectionKey key);
 
-    /**
-     * If the selector contains any listening socket, acceptOp method must be implemented!
-     *
-     * @return Message
-     */
-    protected Message receive() {
-        //Selector must not hold acceptable or writable sockets
-        while (true) {
-            try {
-                selector.select();
-                Iterator<SelectionKey> it = selector.selectedKeys().iterator();
-                SelectionKey key = it.next();
-                it.remove();    //Remove the key from selected-keys set
-                if (key.isAcceptable()) {
-                    acceptOp(key);
-                    continue;
-                }
-                //Invariant: every key must be readable
-                SocketChannel socketChannel = (SocketChannel) key.channel();
-                ByteBuffer byteBuffer = ByteBuffer.allocateDirect(bufferSize);
-                socketChannel.read(byteBuffer);
+	/**
+	 * If the selector contains any listening socket, acceptOp method must be implemented!
+	 *
+	 * @return Message
+	 */
+	protected Message receive() {
+		//Selector must not hold acceptable or writable sockets
+		while (true) {
+			try {
+				selector.select();
+				Iterator<SelectionKey> it = selector.selectedKeys().iterator();
+				SelectionKey key = it.next();
+				it.remove();    //Remove the key from selected-keys set
+				if (key.isAcceptable()) {
+					acceptOp(key);
+					continue;
+				}
+				//Invariant: every key must be readable
+				SocketChannel socketChannel = (SocketChannel) key.channel();
+				ByteBuffer byteBuffer = ByteBuffer.allocateDirect(bufferSize);
+				socketChannel.read(byteBuffer);
 
-                return (Message) deserialize(byteBuffer.array());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+				return (Message) deserialize(byteBuffer.array());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 
 }
