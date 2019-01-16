18d7c5cce5f94d33ff43dc9694342ab274896e3d
문준오 <mjo970625@gmail.com>
2019-01-16 15:57:55 +0900

    Add unit test for find messages

--- src/test/java/kr/ac/hongik/apl/LoggerTest.java
+++ src/test/java/kr/ac/hongik/apl/LoggerTest.java
@@ -7,7 +7,11 @@ import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.sql.SQLException;
 import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SampleOperation extends Operation {
 
@@ -26,6 +30,7 @@ class LoggerTest {
     @Test
     void createTables() {
         Logger logger = new Logger();
+        logger.deleteDBFile();
     }
 
     @Test
@@ -50,9 +55,47 @@ class LoggerTest {
         logger.insertMessage(prepareMessage);
         logger.insertMessage(commitMessage);
 
+        logger.deleteDBFile();
+
     }
 
     @Test
-    void findMessage() {
+    void findMessage() throws NoSuchAlgorithmException, SQLException {
+        Logger logger = new Logger();
+
+        KeyPair keyPair = Util.generateKeyPair();
+        PrivateKey privateKey = keyPair.getPrivate();
+        PublicKey publicKey = keyPair.getPublic();
+
+        Operation sampleOperation = new SampleOperation(
+                new InetSocketAddress("0.0.0.0", 0),
+                Instant.now().getEpochSecond());
+
+        RequestMessage requestMessage = new RequestMessage(privateKey, sampleOperation);
+        PreprepareMessage preprepareMessage = new PreprepareMessage(privateKey, 0, 0, sampleOperation);
+        PrepareMessage prepareMessage = new PrepareMessage(privateKey, 0, 0, "hi", 0);
+        CommitMessage commitMessage = new CommitMessage(privateKey, 0, 0, "hi", 0);
+
+        logger.insertMessage(requestMessage);
+        logger.insertMessage(preprepareMessage);
+        logger.insertMessage(prepareMessage);
+        logger.insertMessage(commitMessage);
+
+        assertTrue(logger.findMessage(commitMessage));
+        assertTrue(logger.findMessage(prepareMessage));
+        assertTrue(logger.findMessage(preprepareMessage));
+        assertTrue(logger.findMessage(requestMessage));
+
+        RequestMessage requestMessage2 = new RequestMessage(privateKey, sampleOperation);
+        PreprepareMessage preprepareMessage2 = new PreprepareMessage(privateKey, 1, 1, sampleOperation);
+        PrepareMessage prepareMessage2 = new PrepareMessage(privateKey, 1, 1, "hello", 1);
+        CommitMessage commitMessage2 = new CommitMessage(privateKey, 1, 1, "hello", 1);
+
+        assertFalse(logger.findMessage(requestMessage2));
+        assertFalse(logger.findMessage(preprepareMessage2));
+        assertFalse(logger.findMessage(prepareMessage2));
+        assertFalse(logger.findMessage(commitMessage2));
+
+        logger.deleteDBFile();
     }
 }
