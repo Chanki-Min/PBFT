4be3a005d8afb9a7afcd35faec46b3e6305f7732
문준오 <mjo970625@gmail.com>
2019-01-17 11:52:40 +0900

    Close #14: Implement pre-prepare phase

--- src/main/java/kr/ac/hongik/apl/Replica.java
+++ src/main/java/kr/ac/hongik/apl/Replica.java
@@ -6,6 +6,9 @@ import java.net.InetSocketAddress;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.Properties;
 
@@ -19,9 +22,13 @@ public class Replica extends Connector implements Primary, Backup {
     ServerSocketChannel serverSocketChannel;
     List<SocketChannel> clients;
 
+    private Logger logger;
+
 
     public Replica(Properties prop, String serverIp, int serverPort) {
         super(prop);
+
+        this.logger = new Logger();
 
         this.myNumber = getMyNumberFromProperty(prop, serverIp, serverPort);
 
@@ -73,12 +80,26 @@ public class Replica extends Connector implements Primary, Backup {
 
     }
 
+    private int getLatestSequenceNumber() throws SQLException {
+        String query = "SELECT MAX(P.seqNum) FROM Preprepares P";
+        PreparedStatement pstmt = logger.getPreparedStatement(query);
+        ResultSet ret = pstmt.executeQuery();
+
+        if (ret.next())  //Normal condition
+            return ret.getInt(1);
+        else            //Initial condition
+            return 0;
+    }
+
     public void startPrepreparePhase(RequestMessage message) {
-		// TODO: sequence number는 어디를 기준으로 증가하여 사용할 것 인가?
-		int seqNum;
-		PreprepareMessage preprepareMessage = new PreprepareMessage(this.getPrivateKey(), this.primary, seqNum, message.getOperation());
-        //Broadcast messages
-        addresses.parallelStream().forEach(address -> send(address, preprepareMessage));
+        try {
+            int seqNum = getLatestSequenceNumber() + 1;
+            PreprepareMessage preprepareMessage = new PreprepareMessage(this.getPrivateKey(), this.primary, seqNum, message.getOperation());
+            //Broadcast messages
+            addresses.parallelStream().forEach(address -> send(address, preprepareMessage));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void broadcastPrepare(Message message) {
@@ -89,7 +110,6 @@ public class Replica extends Connector implements Primary, Backup {
     protected void acceptOp(SelectionKey key) {
         SocketChannel channel = (SocketChannel) key.channel();
         clients.add(channel);
-
     }
 
     public void broadcastViewChange(Message message) {
