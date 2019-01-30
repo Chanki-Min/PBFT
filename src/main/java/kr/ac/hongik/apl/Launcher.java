9f7490ee2620785935af63c7624b51b627c45957
문준오 <mjo970625@gmail.com>
2019-01-30 13:01:08 +0900

    Resolve maven packaging issue

---
+++ src/main/java/kr/ac/hongik/apl/Launcher.java
@@ -0,0 +1,29 @@
+package kr.ac.hongik.apl;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Launcher {
+    public static void main(String[] args) {
+        try {
+            switch (args[0].toLowerCase()) {
+                case "server":
+                    Replica.main(Arrays.stream(args).skip(1).toArray(String[]::new));
+                    break;
+                case "client":
+                    Client.main(Arrays.stream(args).skip(1).toArray(String[]::new));
+                    break;
+                default:
+                    throw new IllegalArgumentException();
+            }
+        } catch (IndexOutOfBoundsException | IllegalArgumentException | IOException e) {
+            String name = new File(Launcher.class.getProtectionDomain()
+                    .getCodeSource()
+                    .getLocation()
+                    .getPath()
+            ).getName();
+            System.err.printf("Usage: java -jar %s server|client [<ip> <port>]\n", name);
+        }
+    }
+}
