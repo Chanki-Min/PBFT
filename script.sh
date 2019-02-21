524e0a99a2848077bb0772c17bd305080a9843ec
Minkyun Kim <mkk0618@gmail.com>
2019-02-21 00:28:39 +0000

    Stop running the processes if packaging failed

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 pkill java
 mvn package
+if [ $? -ne "0" ]
+then
+	exit
+fi
 for i in $(echo "19030 19031 19032")
 do
    java -jar target/PBFT-0.5-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.127 $i &
