92061bc84b71afb0dc4cb6baebd1f0cb4a418bba
Moon Junoh <mjo970625@gmail.com>
2019-06-19 14:28:56 +0900

    Update IP address

--- script.sh
+++ script.sh
@@ -2,14 +2,14 @@
 
 mvn package
 
-scp -r target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar apl@223.194.70.127:~/PBFT/target/
+scp -r ./target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar apl@223.194.70.105:~/PBFT/target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar 
 
-ssh -f  apl@223.194.70.127 '\
+ssh -f  apl@223.194.70.105 '\
 	cd PBFT
 	rm -f *.db
 	pkill java
-	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.127 19030 2>&1 &
+	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19030 2>&1 &
 	for i in $(echo "19031 19032 19033")
 	do
-		java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.127 $i 2>&1 > /dev/null &
+		java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 $i 2>&1 > /dev/null &
 	done'
