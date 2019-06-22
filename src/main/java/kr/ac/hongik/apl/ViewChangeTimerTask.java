47e80027fbde61e9cbbf3f67ff88dcf727ab25c8
Moon Junoh <mjo970625@gmail.com>
2019-06-22 23:17:16 +0900

    Write ViewChangeTimerTask class
    It will be invoked when the timer is expired.
    When the timer is expired, a viewChangeTimerTask instance will create view-change message and broadcast to every replica.

---
+++ src/main/java/kr/ac/hongik/apl/ViewChangeTimerTask.java
@@ -0,0 +1,24 @@
+package kr.ac.hongik.apl;
+
+import static com.diffplug.common.base.Errors.rethrow;
+
+public class ViewChangeTimerTask extends java.util.TimerTask {
+	final int checkpointNum;
+	final int newViewNum;
+	final Replica replica;
+
+	public ViewChangeTimerTask(int checkpointNum, int newViewNum, Replica replica) {
+		this.replica = replica;
+		this.checkpointNum = checkpointNum;
+		this.newViewNum = newViewNum;
+	}
+
+
+	@Override
+	public void run() {
+		var getPreparedStatementFn = rethrow().wrap(replica.getLogger()::getPreparedStatement);
+		ViewChangeMessage viewChangeMessage = ViewChangeMessage.makeViewChangeMsg(replica.getPrivateKey(), checkpointNum, newViewNum, replica.getMyNumber(), getPreparedStatementFn);
+
+		replica.replicas.values().forEach(sock -> replica.send(sock, viewChangeMessage));
+	}
+}
