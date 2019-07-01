1a07f781df3b453c8358a72a91e3454d40689446
yjm6560 <yjm7558@nate,com>
2019-07-01 19:04:59 +0900

    Add GarbageCollection test (to be modified)

--- script.sh
+++ script.sh
@@ -9,7 +9,11 @@ ssh -f  apl@223.194.70.105 '\
 	rm -f *.db
 	pkill --signal 9 -f "java -jar PBFT"
 	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19030 2>&1 &
-	for i in $(echo "19031 19032 19033")
-	do
-		java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 $i 2>&1 > /dev/null &
-	done'
+	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19031 2>&1 &
+	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19032 2>&1 &
+	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19033 2>&1 &
+	'
+#	for i in $(echo "19031 19032 19033")
+#	do
+#		java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 $i 2>&1 > /dev/null &
+#	done'
