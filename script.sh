f73dde70840098090864fd0927b786a5a023470d
Moon Junoh <mjo970625@gmail.com>
2019-07-03 00:59:37 +0900

    Refine script incredibly

--- script.sh
+++ script.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+mvn -T4 package
+scp target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar apl@223.194.70.105:~/
 
-mvn package && \
+tmux -2  new -s PBFT \; \
+	 send-keys "sed 's/PORT/19030/' script.sh | sed -n '19,\$p' | ssh apl@223.194.70.105 \" bash -s \" " C-m \; \
+     split-window -v -p 50 \; \
+	 send-keys "sed 's/PORT/19031/' script.sh | sed -n '19,\$p' | ssh apl@223.194.70.105 \" bash -s \" " C-m \; \
+	 split-window -h -p 50 \; \
+	 send-keys "sed 's/PORT/19032/' script.sh | sed -n '19,\$p' | ssh apl@223.194.70.105 \" bash -s \" " C-m \; \
+	 split-window -h -p 50 \; \
+	 send-keys "sed 's/PORT/19033/' script.sh | sed -n '19,\$p' | ssh apl@223.194.70.105 \" bash -s \" " C-m \; \
+	 select-layout tiled
+exit
 
-scp -r ./target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar apl@223.194.70.105:~/PBFT/target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar && \
 
-ssh -f  apl@223.194.70.105 '\
-	cd PBFT
-	rm -f *.db
-	pkill --signal 9 -f "java -jar PBFT"
-	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19030 2>&1 &
-	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19031 2>&1 &
-	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19032 2>&1 &
-	java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 19033 2>&1 &
-	'
-#	for i in $(echo "19031 19032 19033")
-#	do
-#		java -jar target/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 $i 2>&1 > /dev/null &
-#	done'
+rm -f ~/*.db 
+pkill -9 -f 'java -jar' 
+sleep 1
+java -jar ~/PBFT-1.0-SNAPSHOT-jar-with-dependencies.jar server 223.194.70.105 PORT
