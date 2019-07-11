42693cd34db589727fa7544e8b2aef63d5b67d70
yjm6560 <yjm7558@nate,com>
2019-07-11 13:55:44 +0900

    Remove test case using message in LoggerTest

--- src/test/java/kr/ac/hongik/apl/LoggerTest.java
+++ src/test/java/kr/ac/hongik/apl/LoggerTest.java
@@ -1,8 +1,5 @@
 package kr.ac.hongik.apl;
 
-import kr.ac.hongik.apl.Messages.CommitMessage;
-import kr.ac.hongik.apl.Messages.PrepareMessage;
-import kr.ac.hongik.apl.Messages.PreprepareMessage;
 import kr.ac.hongik.apl.Messages.RequestMessage;
 import kr.ac.hongik.apl.Operations.CountMsgsOperation;
 import kr.ac.hongik.apl.Operations.Operation;
@@ -10,19 +7,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.InputStream;
-import java.security.KeyPair;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.sql.SQLException;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
-
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SampleOperation extends Operation {
 
@@ -43,167 +33,10 @@ class LoggerTest {
         Logger logger = new Logger();
 
     }
-
     @Test
-    void insertPreprepareMessage(){
-        Logger logger = new Logger();
-
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            PreprepareMessage preprepareMessage = PreprepareMessage.makePrePrepareMsg(privateKey, 0, 0, sampleOperation);
-
-            logger.insertMessage(preprepareMessage);
-            logger.insertMessage(preprepareMessage);
-
-        } finally {
-
-        }
-    }
-
-    @Test
-	void insertMessage() {
-        Logger logger = new Logger();
-
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            RequestMessage requestMessage = RequestMessage.makeRequestMsg(privateKey, sampleOperation);
-            PreprepareMessage preprepareMessage = PreprepareMessage.makePrePrepareMsg(privateKey, 0, 0, sampleOperation);
-            PrepareMessage prepareMessage = PrepareMessage.makePrepareMsg(privateKey, 0, 0, "hi", 0);
-            CommitMessage commitMessage = CommitMessage.makeCommitMsg(privateKey, 0, 0, "hi", 0);
-
-            logger.insertMessage(requestMessage);
-            logger.insertMessage(preprepareMessage);
-            logger.insertMessage(prepareMessage);
-            logger.insertMessage(commitMessage);
-        } finally {
-
-        }
-    }
-
-    @Test
-    void findRequestMessage() throws SQLException, NoSuchAlgorithmException {
-
-        Logger logger = new Logger();
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            RequestMessage requestMessage = RequestMessage.makeRequestMsg(privateKey, sampleOperation);
-
-            logger.insertMessage(requestMessage);
-
-            assertTrue(logger.findMessage(requestMessage));
-
-            Operation sampleOperation2 = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond()
-            );
-            RequestMessage requestMessage2 = RequestMessage.makeRequestMsg(privateKey, sampleOperation2);
-
-            assertFalse(logger.findMessage(requestMessage2));
-        } finally {
-
-        }
-    }
-
-    @Test
-    void findPreprepareMessage() throws NoSuchAlgorithmException, SQLException {
-
-        Logger logger = new Logger();
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            PreprepareMessage preprepareMessage = PreprepareMessage.makePrePrepareMsg(privateKey, 0, 0, sampleOperation);
-
-            logger.insertMessage(preprepareMessage);
-
-            assertTrue(logger.findMessage(preprepareMessage));
-
-            PreprepareMessage preprepareMessage2 = PreprepareMessage.makePrePrepareMsg(privateKey, 1, 1, sampleOperation);
-
-            assertFalse(logger.findMessage(preprepareMessage2));
-        } finally {
-
-        }
-    }
-
-    @Test
-    void findPrepareMessage() throws NoSuchAlgorithmException, SQLException {
-
-        Logger logger = new Logger();
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            PrepareMessage prepareMessage = PrepareMessage.makePrepareMsg(privateKey, 0, 0, "hi", 0);
-
-            logger.insertMessage(prepareMessage);
-
-            assertTrue(logger.findMessage(prepareMessage));
-
-            PrepareMessage prepareMessage2 = PrepareMessage.makePrepareMsg(privateKey, 1, 1, "hello", 1);
-
-            assertFalse(logger.findMessage(prepareMessage2));
-        } finally {
-
-        }
-    }
-
-    @Test
-    void findCommitMessage() throws NoSuchAlgorithmException, SQLException {
-
-        Logger logger = new Logger();
-        try {
-            KeyPair keyPair = Util.generateKeyPair();
-            PrivateKey privateKey = keyPair.getPrivate();
-            PublicKey publicKey = keyPair.getPublic();
-
-            Operation sampleOperation = new SampleOperation(
-                    publicKey,
-                    Instant.now().getEpochSecond());
-
-            CommitMessage commitMessage = CommitMessage.makeCommitMsg(privateKey, 0, 0, "hi", 0);
-
-            logger.insertMessage(commitMessage);
-
-            assertTrue(logger.findMessage(commitMessage));
-
-            CommitMessage commitMessage2 = CommitMessage.makeCommitMsg(privateKey, 1, 1, "hello", 1);
-
-            assertFalse(logger.findMessage(commitMessage2));
-        } finally {
-
-        }
+    public void ManyManyClientGC() {
+        for (int i = 0; i < 15; i++)
+            ManyClientGC();
     }
 
     @Test
@@ -241,11 +74,6 @@ class LoggerTest {
         }
     }
 
-    @Test
-    public void ManyManyClientGC(){
-        for (int i = 0; i < 15; i++)
-            ManyClientGC();
-    }
 
     @Test
     public void ManyClientGC() {
