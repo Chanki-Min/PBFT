81db631b7112f2c4db7db22053c1bf47811c0f32
Chanki_Min <alscksrl2010@gmail.com>
2020-04-21 23:17:26 +0900

    Add property key to PropertyNames class

--- src/main/java/kr/ac/hongik/apl/PropertyNames.java
+++ src/main/java/kr/ac/hongik/apl/PropertyNames.java
@@ -4,4 +4,6 @@ public class PropertyNames {
 	public static final String REPLICA_VIEWCHANGE_TIMEOUT_MILLIS = "replica.viewchange.timeout.millis";
 	public static final String REPLICA_GC_WATERMARK_UNIT = "replica.gc.watermark.unit";
 	public static final String REPLICA_MEASURE = "replica.measure";
+	public static final String REPLICA_PRIVATEKEY_PATH = "replica#.privateKey.path";
+	public static final String REPLICA_PUBLICKEY_PATH = "replica#.publicKey.path";
 }
